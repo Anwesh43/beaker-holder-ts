965a8eed523c12dfb6ff5166db3dc17b0d88e6f6
Anwesh43 <anweshthecool0@gmail.com>
2021-06-25 16:51:58 +0530

    adding useStyle hooks

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,4 +1,5 @@
 import {
+    CSSProperties,
     useEffect,
     useState
 } from 'react'
@@ -66,3 +67,75 @@ const divideScale = (
 const sinify = (
     scale : number
     ) : number => Math.sin(scale * Math.PI)
+
+
+const useStyle = (w : number, h : number, scale : number) => {
+    const size : number = Math.min(w, h) / 10
+    const lSize : number = Math.min(w, h) / 3
+    const position = 'absolute'
+    const background : string = "indigo"
+    const sf : number = sinify(scale)
+    const sf1 : number = divideScale(sf, 0, 2)
+    const sf2 : number = divideScale(sf, 1, 2)
+    return {
+        parentStyle() {
+            const left : string = `${w / 2}px`
+            const top : string = `${h / 2}px`
+            const transform = `rotate(${180 * sf1}deg)`
+            return {
+                position, 
+                left, 
+                top, 
+                transform 
+            }
+        },
+        lineStyle() {
+            const left = `${-lSize}px`
+            const top = `${-Math.min(w, h) / 180}px`
+            const width = `${lSize}px`
+            const height = `${Math.min(w,  h) / 90}px` 
+            return {
+                position, 
+                left, 
+                top, 
+                width, 
+                height, 
+                background
+            }
+        },  
+
+        containerStyle() : CSSProperties {
+            const left = `${-lSize}px`
+            const top = `${-size}px`
+            const borderTop = `${size}px solid ${background}`
+            const borderLeft = `${size}px solid ${background}`
+            const borderRight = `${size}px solid ${background}`
+            const borderBottom = `none`
+            return {
+                left, 
+                top, 
+                borderTop, 
+                borderLeft, 
+                borderRight,
+                borderBottom
+            }
+        },
+
+        ballStyle() : CSSProperties {
+            const borderRadius = '50%'
+            const left = `${lSize}px`
+            const top = `${-(h / 2) * (1 - sf2)}px`
+            const width = `${size}px`
+            const height = `${size}px`
+            return {
+                position, 
+                left, 
+                top, 
+                width, 
+                height, 
+                borderRadius
+            }
+        }
+
+    }
+}
